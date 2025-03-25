c33e1ba921e3358f3a891752a2765b18ff200ea1
ncrout1 <ncrout1999@gmail.com>
2025-03-25 12:22:43 +0530

    checking whether one array is another or not 😁

---
+++ 25march_v1.java
@@ -0,0 +1,42 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class narsingh{
+    boolean checkPresence(int [] a, int [] b)
+    {
+        int firstSize=a.length;
+        int secondSize=b.length;
+        boolean checkPresence;
+        
+        for (int i=0; i<secondSize;i++)
+        {    checkPresence=false;
+            for (int j=0;j<firstSize;j++)
+            {
+                if (b[i]==a[j])
+                {
+                    checkPresence=true;
+                    break;
+                }
+            }
+            if (!checkPresence)
+            return false;
+            
+        }
+        return true;
+    }
+    
+    
+    public static void main(String [] args)
+    {
+        
+        int [] a={1,2,3,4};
+        int [] b={6,7};
+         
+         narsingh obj = new narsingh();
+         obj.checkPresence(a,b);
+         System.out.print(obj.checkPresence(a,b));
+        
+        
+    }
+    
+}
