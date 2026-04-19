54903c885b147390ca741e0d74d77c611bd8c6dc
ncrout1 <ncrout1999@gmail.com>
2026-04-19 16:41:45 +0530

    Add Main class to print characters of a string

---
+++ 19thApril2026.java
@@ -0,0 +1,15 @@
+class Main{
+
+public static void main(String[] args)
+  {
+
+    String nars ="KeshavBhai";
+    char []a=nars.toCharArray();
+    for (int i=0;i<a.length;i++)
+      {
+        System.out.println(a[i]);
+      }
+  }
+
+  
+}
